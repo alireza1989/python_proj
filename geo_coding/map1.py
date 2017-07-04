67ab1c4d6adb7b849421c2bdff92b6c52a83e94c
Alireza Darbehani <alireza.darbehani@gmail.com>
2017-07-04 11:34:32 -0700

    Add markers from CSV file

--- geo_coding/map1.py
+++ geo_coding/map1.py
@@ -1,9 +1,15 @@
 import folium
+import pandas
+
+data = pandas.read_csv("Volcanoes.txt")
+lat = list(data["LAT"])
+lon = list(data["LON"])
 
 map = folium.Map(location=[38.58,-99.09], zoom_start=6, tiles="Mapbox Bright")
-
 fg = folium.FeatureGroup(name="My Map")
-fg.add_child(folium.Marker(location=[38.2, -99.1], popup="I am a marker", icon=folium.Icon(color="green")))
-map.add_child(fg)
 
+for lt, ln in zip(lat, lon):
+    fg.add_child(folium.Marker(location=[lt, ln], popup="I am a marker", icon=folium.Icon(color="green")))
+
+map.add_child(fg)
 map.save("map1.html")
