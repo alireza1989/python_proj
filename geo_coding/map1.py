2f6728c3247d4d741f2509cc14d69aee5a4fea4d
Alireza Darbehani <alireza.darbehani@gmail.com>
2017-07-04 12:07:59 -0700

    add color base markers

--- geo_coding/map1.py
+++ geo_coding/map1.py
@@ -4,12 +4,21 @@ import pandas
 data = pandas.read_csv("Volcanoes.txt")
 lat = list(data["LAT"])
 lon = list(data["LON"])
+elev = list(data["ELEV"])
+
+def color_producer(elevation):
+    if elevation < 1000:
+        return 'green'
+    elif 1000 <= elevation < 3000:
+        return 'orange'
+    else:
+        return 'red'
 
 map = folium.Map(location=[38.58,-99.09], zoom_start=6, tiles="Mapbox Bright")
 fg = folium.FeatureGroup(name="My Map")
 
-for lt, ln in zip(lat, lon):
-    fg.add_child(folium.Marker(location=[lt, ln], popup="I am a marker", icon=folium.Icon(color="green")))
+for lt, ln, el in zip(lat, lon, elev):
+    fg.add_child(folium.Marker(location=[lt, ln], popup=str(el) + " m", icon=folium.Icon(color_producer(el))))
 
 map.add_child(fg)
 map.save("map1.html")
