56b87b3f4427895df056c9307142e0f05b1697cc
Alireza Darbehani <alireza.darbehani@gmail.com>
2017-07-04 11:09:47 -0700

    add feature group

--- geo_coding/map1.py
+++ geo_coding/map1.py
@@ -2,5 +2,8 @@ import folium
 
 map = folium.Map(location=[38.58,-99.09], zoom_start=6, tiles="Mapbox Bright")
 
-map.add_child(folium.Marker(location=[38.2, -99.1], popup="I am a marker", icon=folium.Icon(color="green")))
+fg = folium.FeatureGroup(name="My Map")
+fg.add_child(folium.Marker(location=[38.2, -99.1], popup="I am a marker", icon=folium.Icon(color="green")))
+map.add_child(fg)
+
 map.save("map1.html")
