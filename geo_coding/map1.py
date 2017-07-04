8516f412649ec77b1c8403182c2d7f74a94b65aa
Alireza Darbehani <alireza.darbehani@gmail.com>
2017-07-04 11:06:14 -0700

    create basic map

---
+++ geo_coding/map1.py
@@ -0,0 +1,6 @@
+import folium
+
+map = folium.Map(location=[38.58,-99.09], zoom_start=6, tiles="Mapbox Bright")
+
+map.add_child(folium.Marker(location=[38.2, -99.1], popup="I am a marker", icon=folium.Icon(color="green")))
+map.save("map1.html")
