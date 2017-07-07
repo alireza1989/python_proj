9b9537a81601e8852525211a69ae7079f6ea5b28
Alireza Darbehani <alireza.darbehani@gmail.com>
2017-07-06 17:16:23 -0700

    Add Polygons and Layer CP

--- geo_coding/map1.py
+++ geo_coding/map1.py
@@ -14,11 +14,19 @@ def color_producer(elevation):
     else:
         return 'red'
 
-map = folium.Map(location=[38.58,-99.09], zoom_start=6, tiles="Mapbox Bright")
-fg = folium.FeatureGroup(name="My Map")
+mapp = folium.Map(location=[38.58,-99.09], zoom_start=6, tiles="Mapbox Bright")
+fgv = folium.FeatureGroup(name="Volcanoes")
+fgp = folium.FeatureGroup(name="Population")
 
 for lt, ln, el in zip(lat, lon, elev):
-    fg.add_child(folium.Marker(location=[lt, ln], popup=str(el) + " m", icon=folium.Icon(color_producer(el))))
+    fgv.add_child(folium.CircleMarker(location=[lt, ln], popup=str(el) + " m", radius=6, fill_color=color_producer(el), color='gray', fill_opacity=0.7))
 
-map.add_child(fg)
-map.save("map1.html")
+fgp.add_child(folium.GeoJson(data=open('world.json', 'r', encoding='utf-8-sig'),
+style_function=lambda x: {'fillColor':'green' if x['properties']['POP2005'] < 10000000
+else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red'}))
+
+
+mapp.add_child(fgv)
+mapp.add_child(fgp)
+mapp.add_child(folium.LayerControl())
+mapp.save("map1.html")
